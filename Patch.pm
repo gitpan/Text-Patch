31b94e95872d4737310716d48ddf8720d657c58b
Vladi Belperchinov-Shabanski <cade@datamax.bg>
2007-04-07 19:12:20 +0000

    Import of CADE/Text-Patch-1.3 from CPAN.
    gitpan-cpan-distribution: Text-Patch
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         CADE/Text-Patch-1.3.tar.gz
    gitpan-cpan-author:       CADE
    gitpan-cpan-maturity:     released

--- Patch.pm
+++ Patch.pm
@@ -1,55 +1,67 @@
-#!/usr/bin/perl
 package Text::Patch;
 use Exporter;
 our @ISA = qw( Exporter );
 our @EXPORT = qw( patch );
-our $VERSION = '1.2';
+our $VERSION = '1.3';
 use strict;
 use warnings;
 use Carp;
+
+use constant NO_NEWLINE => '\\ No newline at end of file';
 
 sub patch
 {
   my $text = shift;
   my $diff = shift;
-  my %options;
+  my %options = ref $_[0] eq 'HASH' ? %{ $_[0] } : @_;
 
-  if( ref $_[0] eq 'HASH' )
-    {
-    %options = %{ $_[0] };
-    }
-  else
-    {
-    %options = @_;
-    }
-
-  return patch_unified( $text, $diff ) if $options{ 'STYLE' } eq 'Unified';
-  croak "required STYLE option is missing";
-}
-
-sub patch_unified
-{
-  my $text = shift;
-  my $diff = shift;
+  my %handler = ('unified'  => \&patch_unified,
+                 'context'  => \&patch_context,
+                 'oldstyle' => \&patch_oldstyle,
+                 );
+  my $style = $options{STYLE};
+  croak "required STYLE option is missing" unless $style;
+  croak "source required" unless defined $text;
+  croak "diff required" unless defined $diff;
+  my $code = $handler{lc($style)} || croak "unrecognised STYLE '$style'";
 
   my @text = split /^/m, $text;
   my @diff = split /^/m, $diff;
 
+  # analyse source/diff to determine line ending used.
+  # (if source is only 1 line, can't use it to determine line endings)
+  my $line1 = @text > 1 ? $text[0] : $diff[0];
+  my($line1c, $sep) = _chomp($line1);
+  $sep ||= "\n";  # default to unix line ending
+
+  # apply patch
+  DUMP("got patch", \@diff);
+  my $out = $code->(\@text, \@diff, $sep);
+
+  my $lastline = _chomp($diff[-1], $sep);
+  $out = _chomp($out, $sep) if $lastline eq NO_NEWLINE;
+  return $out;
+}
+
+sub patch_unified
+{
+  my($text, $diff, $sep) = @_;
   my @hunks;
   my %hunk;
 
-  for( @diff )
+  for( @$diff )
     {
     #print STDERR ">>> ... $_";
-    if( /^\@\@\s*-(\d+),(\d+)/ )
+    if( /^\@\@\s*-([\d,]+)/ )
       {
       #print STDERR ">>> *** HUNK!\n";
+      my($pos1, $count1) = split /,/, $1;
       push @hunks, { %hunk };
       %hunk = ();
-      $hunk{ FROM } = $1 - 1; # diff is 1-based
+      $hunk{ FROM } = $pos1 - 1; # diff is 1-based
       # Modification by Ben L., patches may have @@ -0,0 if the source is empty.
       $hunk{ FROM } = 0 if $hunk{ FROM } < 0;
-      $hunk{ LEN  } = $2;
+      $hunk{ LEN  } = defined $count1 ? $count1 : $pos1 == 0 ? 0 : 1;
       $hunk{ DATA } = [];
       }
     push @{ $hunk{ DATA } }, $_;
@@ -57,23 +69,177 @@ sub patch_unified
   push @hunks, { %hunk }; # push last hunk
   shift @hunks; # first is always empty
 
-  for my $hunk ( reverse @hunks )
+  return _patch($text, \@hunks, $sep);
+}
+
+sub patch_oldstyle {
+    my($text, $diff, $sep) = @_;
+    my @hunks;
+    my $i = 0;
+
+    my $hunk_head = qr/^([\d,]+)([acd])([\d,]+)$/;
+    while($i < @$diff) {
+        my $l = $diff->[$i];
+        my($r1, $type, $r2) = $l =~ $hunk_head;
+        die "Malformed patch at line ".($i + 1)."\n"
+            unless defined $r1 && $type && defined $r2;
+        my($pos1, $count1) = _range($r1);
+        my($pos2, $count2) = _range($r2);
+
+        # parse chunk data
+        my @data;
+        my $j = $i + 1;
+        for(; $j < @$diff; $j++) {
+            $l = $diff->[$j];
+            last if $l =~ $hunk_head;
+            next if $l =~ /^---/;  # separator
+            push @data, $l;
+        }
+        my $datalen = $j - $i - 1;
+
+        if($type eq 'a') { # add
+            $count1 = 0;   # don't remove any lines
+            $pos1++;       # add to line after pos1
+        }
+
+        # convert data to a format _patch() will understand
+        for(@data) {
+            $_ =~ s/^< /-/;
+            $_ =~ s/^> /+/;
+        }
+
+        push @hunks, { FROM => $pos1 - 1,
+                       LEN  => $count1,
+                       DATA => \@data,
+                     };
+        $i += $datalen + 1;
+    }
+    return _patch($text, \@hunks, $sep);
+}
+
+# NB: this works by converting hunks into a kind of unified format
+sub patch_context {
+    my($text, $diff, $sep) = @_;
+    my $i = 0;
+    my @hunks;
+
+    # skip past header
+    for(@$diff) {
+        $i++;
+        last if /^\Q***************\E$/;  # end header marker
+    }
+
+    # this sub reads one half of a hunk (from/to part)
+    my $read_part = sub {
+        my $l = $diff->[$i++];
+        TRACE("got line: $l");
+        die "Malformed patch at line $i\n"
+            unless $l =~ /^(?:\*\*\*|---)\s+([\d,]+)\s+(?:\*\*\*|---)/;
+        my($pos, $count) = _range($1);
+        my @part;
+        while($i < @$diff) {
+            my $l = $diff->[$i];
+            last if $l =~ /^(\*\*\*|---)/;
+            push @part, $l;
+            $i++;
+        }
+        DUMP("got part", \@part);
+        return (\@part, $pos, $count);
+    };
+
+    while($i < @$diff) {
+        # read the from and to part of this hunk
+        my($part1, $pos1, $count1) = $read_part->();
+        my($part2, $pos2, $count2) = $read_part->();
+        $i++;  # skip chunk separator
+
+        # convert operations to unified style ones
+        $_ =~ s/^(.)\s/$1/ for @$part1, @$part2;
+        $_ =~ s/^\!/-/ for @$part1;  # remove
+        $_ =~ s/^\!/+/ for @$part2;  # add
+
+        # merge the parts to create a unified style chunk
+        my @data;
+        for(;;) {
+            my $c1 = $part1->[0];
+            my $c2 = $part2->[0];
+            last unless defined $c1 || defined $c2;
+
+            if(defined $c1 && $c1 =~ /^-/) {
+                push @data, shift @$part1;  # remove line
+            } elsif(defined $c2 && $c2 =~ /^\+/) {
+                push @data, shift @$part2;  # add line
+            } else {                        # context
+                my($x1, $x2) = (shift @$part1, shift @$part2);
+                push @data, defined $x1 ? $x1 : $x2;
+            }
+        }
+        push @hunks, { FROM => $pos1 - 1,
+                       LEN  => $count1,
+                       DATA => \@data,
+                     };
+        DUMP("merged data", \@data);
+    }
+    return _patch($text, \@hunks, $sep);
+}
+
+######################################################################
+# private
+
+# returns (start line, line count)
+sub _range {
+    my($range) = @_;
+    my($pos1, $pos2) = split /,/, $range;
+    return ($pos1, defined $pos2 ? $pos2 - $pos1 + 1 : 1);
+}
+
+sub _patch {
+  my($text, $hunks, $sep) = @_;
+  my $hunknum = scalar @$hunks + 1;
+  die "No hunks found\n" unless @$hunks;
+  for my $hunk ( reverse @$hunks )
     {
-    #use Data::Dumper;
-    #print STDERR Dumper( $hunk );
+    $hunknum--;
+    DUMP("hunk", $hunk);
+
     my @pdata;
+    my $num = $hunk->{FROM};
     for( @{ $hunk->{ DATA } } )
       {
       next unless s/^([ \-\+])//;
       #print STDERR ">>> ($1) $_";
-      next if $1 eq '-';
-      push @pdata, $_;
+      if($1 ne '+') {
+          # not an addition, check line for match against existing text.
+          # ignore line endings for comparison
+          my $orig   = _chomp($text->[$num++], $sep); # num 0 based here
+          my $expect = _chomp($_, $sep);
+          TRACE("checking >>$orig<< against >>$expect<<");
+          die "Hunk #$hunknum failed at line $num.\n" # actual line number
+              unless $orig eq $expect;
       }
-    splice @text, $hunk->{ FROM }, $hunk->{ LEN }, @pdata;
+      next if $1 eq '-';  # removals
+      push @pdata, $_;    # add/replace line
+      }
+    splice @$text, $hunk->{ FROM }, $hunk->{ LEN }, @pdata;
     }
 
-  return join '', @text;
+  return join '', @$text;
 }
+
+# chomp $sep from the end of line
+# if $sep is not given, chomp unix or dos line ending
+sub _chomp {
+    my($text, $sep) = @_;
+    if($sep) {
+        $text =~ s/($sep)$//;
+    } else {
+        $text =~ s/(\r\n|\n)$//;
+    }
+    return wantarray ? ($text, $1) : $text;
+}
+
+sub DUMP {}
+sub TRACE {}
 
 =pod
 
@@ -120,8 +286,7 @@ Options are:
 
   STYLE => 'Unified'
 
-Note that currently only 'Unified' diff format is supported!
-STYLE names are the same described in Text::Diff.
+STYLE can be "Unified", "Context" or "OldStyle".
 
 The 'Unified' diff format looks like this:
 
@@ -146,14 +311,9 @@ The 'Unified' diff format looks like this:
 
 =back
 
-=head1 LIMITS
-
-  Only 'Unified' diff format is supported.
-
 =head1 TODO
 
   Interfaces with files, arrays, etc.
-  Diff formats support: "Context", "OldStyle" (As noted in Text::Diff)
 
 =head1 AUTHOR
 
@@ -168,4 +328,3 @@ The 'Unified' diff format looks like this:
   $Id: Patch.pm,v 1.4 2007/01/09 00:11:15 cade Exp $
 
 =cut
-
--- test.pl
+++ test.pl
@@ -5,14 +5,17 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test;
-BEGIN { plan tests => 1 };
+use Test::More;
 use strict;
 use Text::Diff;
 use Text::Patch;
 
-my $t1 = '
-The Way that can be told of is not the eternal Way;
+#use Log::Trace;
+#import Log::Trace 'warn' => { Deep => 0 };
+
+my @styles = qw/Unified Context OldStyle/;
+
+my $t1 = 'The Way that can be told of is not the eternal Way;
 The name that can be named is not the eternal name.
 The Nameless is the origin of Heaven and Earth;
 The Named is the mother of all things.
@@ -25,8 +28,7 @@ But after they are produced,
   they have different names.
 ';
 
-my $t2 = '
-The Nameless is the origin of Heaven and Earth;
+my $t2 = 'The Nameless is the origin of Heaven and Earth;
 The named is the mother of all things.
 
 Therefore let there always be non-being,
@@ -41,15 +43,48 @@ Deeper and more profound,
 The door of all subtleties!
 ';
 
-my $patch = diff( \$t1, \$t2, { STYLE => "Unified" } );
+chomp(my $t1b = $t1);
+chomp(my $t2b = $t2);
 
-print "[$patch]\n";
+my @data; # [ text1, text2, style, break, testname, require Text-Diff > 0.35]
 
-my $t3 = patch( $t1, $patch, { STYLE => "Unified" } );
+# test different styles with different data
+for my $style (@styles) {
+    push @data, [$t1,  $t2,  $style, 0, "normal"];
+    push @data, [$t1,  $t2b, $style, 0, "t2 no newline"];
+    push @data, [$t1b, $t2,  $style, 0, "t1 no newline", 1];
+    push @data, [$t1b, $t2b, $style, 0, "t1,t2 no newline", 1];
+}
 
-print "original: [$t2]\npatched: [$t3]";
+# test breaking it with bad hunks
+for my $style (@styles) {
+    push @data, [$t1, $t2, $style, 1, "bad hunk"];
+}
 
-print $t2 eq $t3 ? "\n\n*** YES ***\n\n" : "\n\n*** NO ***\n\n";
+plan tests => scalar @data;
 
-ok(1) if $t2 eq $t3;
+for my $d (@data) {
+    my($test1, $test2, $style, $break, $name, $td_035) = @$d;
+    my $patch = diff( \$test1, \$test2, { STYLE => $style } );
+    $test1 =~ s/(\r\n|\n)/ -- broken --$1/ if $break;
 
+    SKIP: {
+        skip "Text::Diff > 0.35 required", 1
+            if $td_035 && $Text::Diff::VERSION <= 0.35;
+
+        #warn "using patch: >>$patch<<\n";
+        my $test3 = eval { patch( $test1, $patch, { STYLE => $style } ) };
+        my $error = $@;
+        my $testname = "patch $style ($name)";
+        my $ok = $break ? $error : !$error && $test2 eq $test3;
+        unless(ok($ok, "patch $style ($name)")) {
+            diag "error: $error" if $error;
+            DUMP("$style patch", $patch);
+            DUMP("original", $test2);
+            DUMP("patched", $test3);
+        }
+    }
+}
+
+sub TRACE {}
+sub DUMP {}
